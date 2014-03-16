d7e0eb51f0770a1c32c8e2ce8fc0fc3bec08908c
Danny Kopping <dannykopping@gmail.com>
2014-03-16 16:12:53 +0200

    Finishing touches

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,20 @@
-/**
- * Created by dannykopping on 2014/03/01.
- */
+var gulp = require('gulp');
+var browserify = require('gulp-browserify'),
+    uglify = require('gulp-uglify'),
+    notify = require( 'gulp-notify'),
+    size = require('gulp-size');
+
+// Basic usage
+gulp.task('default', function() {
+
+  gulp.watch(['amf.js'], function(e) {
+    gulp.src('amf.js')
+        .pipe(browserify({
+          insertGlobals: true
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist'))
+        .pipe(notify('Done!'))
+        .pipe(size());
+  });
+});
